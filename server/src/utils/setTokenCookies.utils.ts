f3164614af25be3cf90f3221b016ab22241ff408
ASR-01 <gamingasr002@gmail.com>
2024-08-02 19:53:09 +0530

    asf

--- server/src/utils/setTokenCookies.utils.ts
+++ server/src/utils/setTokenCookies.utils.ts
@@ -7,21 +7,21 @@ export const setTokenCookies = async (
 ) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: false,
+    secure: true,
     maxAge: 10 * 60 * 1000, // 10 minutes
     sameSite: "strict",
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: false,
+    secure: true,
     maxAge: 5 * 24 * 60 * 60 * 1000, // 5 days
     sameSite: "strict",
   });
 
   res.cookie("is_Auth", true, {
-    httpOnly: false,
-    secure: false,
+    httpOnly: true,
+    secure: true,
     maxAge: 5 * 24 * 60 * 60 * 1000, // 5 days
     sameSite: "strict",
   });
